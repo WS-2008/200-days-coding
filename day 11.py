59b0681ad11fe21f86cb8c2128bc02d5a182013d
WS-2008 <89751636+WS-2008@users.noreply.github.com>
2025-04-25 22:14:44 +0100

    Create day 11.py

---
+++ day 11.py
@@ -0,0 +1,17 @@
+# Creating a tuple
+info_person = (“Ayo", 28, "Engineer") 
+
+fruits = ["apple", "banana", "cherry"]  # Creating the list
+fruits.append("orange")  # Adding an item
+fruits[1] = "blueberry"  # Changing an item by the index 
+
+
+# creating the range
+
+r = range(2, 8) # 2 to 7
+print(list(r)) # [2, 3, 4, 5, 6, 7]
+
+for i in range(5):  
+    print(i)
+
+# Testing mutability
